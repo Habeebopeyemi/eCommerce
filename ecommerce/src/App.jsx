8c0176e8cef27dc218e6f5fb92c69fc17a871804
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2022-02-05 17:27:59 +0100

    included component such as app, navbar, and main content

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Navbar } from "./Navbar";
+import MainContent from "./MainContent";
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <MainContent />
+      </React.Fragment>
+    );
+  }
+}
--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "@popperjs/core";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
